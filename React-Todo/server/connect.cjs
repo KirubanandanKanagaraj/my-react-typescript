e6ede63ce8f1a91c54be8c962325940592798596
KirubanandanKanagaraj <anandh.kbn@gmail.com>
2025-04-14 23:08:43 +0200

    server setup: readme updated

--- React-Todo/server/connect.cjs
+++ React-Todo/server/connect.cjs
@@ -3,13 +3,14 @@ require("dotenv").config({ path: "./config.env" });
 
 async function main() {
   const Db = process.env.ATLAS_URI;
+  const collectionName = process.env.COLLECTION_NAME;
   const client = new MongoClient(Db);
 
   try {
     await client.connect();
     console.log("Connected to MongoDB Atlas");
 
-    const collections = await client.db("todoList").collections();
+    const collections = await client.db(collectionName).collections();
     collections.forEach((collection) => {
       console.log(`Collection: ${collection.s.namespace.collection}`);
     });
